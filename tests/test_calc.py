c3bb85e028979ddf0bcaea6367bda2a3f129a068
Minh <minh.seikel@gmail.com>
2022-09-21 11:44:01 -0700

    Wave 4 is completed

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -2,14 +2,17 @@ import pytest
 
 from average_calculator.calc import rounded_average
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_average_of_two_numbers_is_properly_computed():
     actual = rounded_average([4, 6])
-    # Delete the skip and write the proper assertion here!
-    pass
+    assert actual == 5
+    # pass
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_average_of_empty_list_raises_ValueError():
+    with pytest.raises(ZeroDivisionError):
+        rounded_average([])
+    # assert act
     # Delete the skip and write the test here!
     # Hint: use pytest.raises
-    pass
+    # pass
